508fc5cd1dba3789e200d6d8022cdb90c002937e
Pop Plays <ng.qi.an@sji.edu.sg>
2023-08-03 20:45:11 +0800

    Initial Commit

---
+++ GST calculator/GST_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GST_calculatorApp.swift
+//  GST calculator
+//
+//  Created by Ng Qi An on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct GST_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
